3282cd4830e1e29af2d836c3b83e3454b937f7a5
Jeffrey Estrada <jestrada321@gmail.com>
2023-04-10 18:57:29 -0400

    Update,Delete, List All

--- pkg/cocktails/cocktails.go
+++ pkg/cocktails/cocktails.go
@@ -7,6 +7,7 @@ import (
 
 	cocktailv1 "github.com/jestradaramos/sho/api/gen/go/v1"
 	"github.com/jestradaramos/sho/pkg/repo"
+	"github.com/jestradaramos/sho/pkg/repo/bun"
 )
 
 // cocktailServiceServer implements the CocktailServiceServer API.
@@ -54,14 +55,29 @@ func (s *CocktailServiceServer) GetCocktail(ctx context.Context, req *cocktailv1
 // ListCocktails Lists all available cocktails
 func (s *CocktailServiceServer) ListCocktails(ctx context.Context, req *cocktailv1.ListCocktailsRequest) (*cocktailv1.ListCocktailsResponse, error) {
 	log.Println("Got a request to list all cocktails")
+	cocktails, err := s.repo.ListCocktail(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-	return &cocktailv1.ListCocktailsResponse{}, nil
+	protoCocktails := []*cocktailv1.Cocktail{}
+
+	for _, c := range *cocktails {
+		pc := fromRepoToProto(c)
+		protoCocktails = append(protoCocktails, &pc)
+	}
+
+	return &cocktailv1.ListCocktailsResponse{Cocktails: protoCocktails}, nil
 }
 
 // UpdateCocktail updates an existing cocktail
 func (s *CocktailServiceServer) UpdateCocktail(ctx context.Context, req *cocktailv1.UpdateCocktailRequest) (*cocktailv1.UpdateCocktailResponse, error) {
 	name := req.Cocktail.Name
 	log.Println("Got a request to update", name)
+	err := s.repo.UpdateCocktail(ctx, req.Cocktail)
+	if err != nil {
+		return nil, err
+	}
 
 	return &cocktailv1.UpdateCocktailResponse{}, nil
 }
@@ -70,6 +86,22 @@ func (s *CocktailServiceServer) UpdateCocktail(ctx context.Context, req *cocktai
 func (s *CocktailServiceServer) DeleteCocktail(ctx context.Context, req *cocktailv1.DeleteCocktailRequest) (*cocktailv1.DeleteCocktailResponse, error) {
 	id := req.Id
 	log.Println("Got a request to delete ", id)
+	err := s.repo.DeleteCocktail(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
 
 	return &cocktailv1.DeleteCocktailResponse{}, nil
+}
+
+func fromRepoToProto(cocktail bun.Cocktail) cocktailv1.Cocktail {
+	p := cocktailv1.Cocktail{
+		Id:          cocktail.ID.String(),
+		Name:        cocktail.Name,
+		Notes:       cocktail.Notes,
+		Ingredients: cocktail.Ingredients,
+		Steps:       cocktail.Steps,
+	}
+
+	return p
 }
