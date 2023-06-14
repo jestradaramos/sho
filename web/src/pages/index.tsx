c6b571342f874a51beced9ac9486bfde875cc79d
Jeffrey Estrada <jestrada321@gmail.com>
2023-06-14 00:26:06 -0400

    adding background image

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Button from '../componenets/common/Button'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +12,19 @@ export default function Home() {
         <title>Jeffy's Bar</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Image 
+        src='/JER09793.jpg'
+        alt='Greenpointe'
+        quality={100}
+        style={{objectFit: "cover"}}
+        fill/>
+      
+
       <Button variant='primary'>Hey</Button>
       <Button variant='secondary'>Hey</Button>
+      
+      
     </>
   )
 }
