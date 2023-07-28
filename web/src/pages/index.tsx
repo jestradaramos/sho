c2a9ef8a658b49f4ca158adb833cae5eb7b5f6ee
Jeffrey Estrada <jestrada321@gmail.com>
2023-07-28 18:25:26 -0400

    remove mui, add navbar, add background image

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,19 +1,43 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Button from '../componenets/button'
+import NavBar from '../componenets/navbar'
 import { BUTTON_TYPES } from '@/common/data/button'
+import background from '../../public/JER09793.jpg'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='home'>
-      <div className='btn-wrapper'>
-        <Button type={`${BUTTON_TYPES.PRIMARY}`} btnText="Primary" />
-        <Button type={`${BUTTON_TYPES.SECONDARY}`} btnText="Secondary" />
-        <Button type={`${BUTTON_TYPES.TERTIARY}`} btnText="Tertiary" />
-      </div>
+    <>
 
-    </div>
+      <div className='base'
+        style={{
+          backgroundImage: `url(${background.src})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          width: '100vw',
+          height: '100vh',
+        }}>
+
+        <NavBar />
+        <div className='home'>
+
+          <div className='btnWrapper'>
+            <Button type={`${BUTTON_TYPES.PRIMARY}`} btnText="Primary" />
+            <Button type={`${BUTTON_TYPES.SECONDARY}`} btnText="Secondary" />
+            <Button type={`${BUTTON_TYPES.TERTIARY}`} btnText="Tertiary" />
+          </div>
+
+        </div>
+      </div>
+    </>
+
+
+
+
+
+
   )
 }
